28a29c1d9ab113b76e502f020af56e127f12811b
Juan Felipe Rojas <juanfeliperojas2@protonmail.com>
2026-03-17 15:31:52 -0500

    Removed manual performance benchmarks

--- main.cpp
+++ main.cpp
@@ -1,8 +1,5 @@
 #include "Kernels.hpp"
 #include "Matrix.hpp"
-#include <chrono>
-#include <cstddef>
-#include <iostream>
 
 int main() {
   size_t N = 2880;
@@ -13,16 +10,5 @@ int main() {
   A.fillRandom();
   B.fillRandom();
 
-  auto start{std::chrono::high_resolution_clock::now()};
   Kernels::matMulParallel<64>(A, B, C);
-  auto end{std::chrono::high_resolution_clock::now()};
-
-  std::chrono::duration<double> diff{end - start};
-  double elapsed{diff.count()};
-  std::cout << "Took: " << elapsed << " s." << '\n';
-
-  double N_val = static_cast<double>(N);
-  double total_flops{2 * N_val * N_val * N_val};
-  double gflops{total_flops / (elapsed * 1e9)};
-  std::cout << "Performance: " << gflops << " GFLOPS" << '\n';
 }
