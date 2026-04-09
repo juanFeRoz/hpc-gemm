4b80eeb12e37f9de63c5f67ffe79fde98ce78ce0
Juan Felipe Rojas <juanfeliperojas2@protonmail.com>
2026-04-09 09:20:14 -0500

    Added blocktiling

--- Kernels.cuh
+++ Kernels.cuh
@@ -2,19 +2,57 @@
 #define KERNELS_CUH
 
 #include <cuda_runtime.h>
-namespace Kernels {
-template <int BLOCKSIZE>
-__global__ void sgemm_naive(int M, int N, int K, const float *A, const float *B,
-                            float *C) {
-  int row = blockIdx.x * BLOCKSIZE + (threadIdx.x / BLOCKSIZE);
-  int col = blockIdx.y * BLOCKSIZE + (threadIdx.x % BLOCKSIZE);
 
-  if (row < M && col < N) {
-    float tmp = 0.0;
-    for (int i = 0; i < K; ++i) {
-      tmp += A[row * K + i] * B[i * N + col];
+namespace Kernels {
+
+template <const int BM, const int BN, const int BK, const int TM>
+__global__ void sgemm1DTiling(int M, int N, int K, const float *A,
+                              const float *B, float *C) {
+  const uint cRow = blockIdx.y;
+  const uint cCol = blockIdx.x;
+
+  const int threadCol = threadIdx.x % BN;
+  const int threadRow = threadIdx.x / BN;
+
+  __shared__ float As[BM * BK];
+  __shared__ float Bs[BK * BN];
+
+  A += cRow * BM * K;
+  B += cCol * BN;
+  C += (cRow * BM + threadRow * TM) * N + cCol * BN;
+
+  const uint innerColA = threadIdx.x % BK;
+  const uint innerRowA = threadIdx.x / BK;
+  const uint innerColB = threadIdx.x % BN;
+  const uint innerRowB = threadIdx.x / BN;
+
+  float threadResults[TM] = {0.0f};
+
+  for (uint bkIdx = 0; bkIdx < K; bkIdx += BK) {
+    As[innerRowA * BK + innerColA] = A[innerRowA * K + innerColA];
+    Bs[innerRowB * BN + innerColB] = B[innerRowB * N + innerColB];
+
+    __syncthreads();
+
+    for (uint dotIdx = 0; dotIdx < BK; ++dotIdx) {
+      float tmpB = Bs[dotIdx * BN + threadCol];
+      for (uint resIdx = 0; resIdx < TM; ++resIdx) {
+        threadResults[resIdx] +=
+            As[(threadRow * TM + resIdx) * BK + dotIdx] * tmpB;
+      }
     }
-    C[row * N + col] = tmp;
+
+    __syncthreads();
+
+    A += BK;
+    B += BK * N;
+  }
+
+  for (uint resIdx = 0; resIdx < TM; ++resIdx) {
+    if ((cRow * BM + threadRow * TM + resIdx) < M &&
+        (cCol * BN + threadCol) < N) {
+      C[resIdx * N + threadCol] = threadResults[resIdx];
+    }
   }
 }
 } // namespace Kernels
--- main.cu
+++ main.cu
@@ -5,7 +5,7 @@
 
 int main(int argc, char *argv[]) {
   size_t N = 512;
-  int blockSize = 8;
+  int blockSize = 16;
   if (argc >= 2)
     N = std::atoi(argv[1]);
   if (argc >= 3)
@@ -18,10 +18,7 @@ int main(int argc, char *argv[]) {
   A.fillRandom();
   B.fillRandom();
 
-  float *dA;
-  float *dB;
-  float *dC;
-
+  float *dA, *dB, *dC;
   size_t size = N * N * sizeof(float);
 
   cudaMalloc(&dA, size);
@@ -31,34 +28,31 @@ int main(int argc, char *argv[]) {
   cudaMemcpy(dA, A.data(), size, cudaMemcpyHostToDevice);
   cudaMemcpy(dB, B.data(), size, cudaMemcpyHostToDevice);
 
-  dim3 dimBlock(blockSize * blockSize);
-  dim3 dimGrid((N + blockSize - 1) / blockSize,
-               (N + blockSize - 1) / blockSize);
-
-  cudaDeviceSynchronize();
-
   const int iterations = 10;
   for (int i = 0; i < iterations; ++i) {
-    switch (blockSize) {
-    case 8:
-      Kernels::sgemm_naive<8><<<dimGrid, dimBlock>>>(N, N, N, dA, dB, dC);
-      break;
-    case 16:
-      Kernels::sgemm_naive<16><<<dimGrid, dimBlock>>>(N, N, N, dA, dB, dC);
-      break;
-    case 32:
-      Kernels::sgemm_naive<32><<<dimGrid, dimBlock>>>(N, N, N, dA, dB, dC);
-      break;
-    default:
-      break;
+    if (blockSize == 8) {
+      dim3 dimBlock((8 * 8) / 8);
+      dim3 dimGrid((N + 8 - 1) / 8, (N + 8 - 1) / 8);
+      Kernels::sgemm1DTiling<8, 8, 8, 8>
+          <<<dimGrid, dimBlock>>>(N, N, N, dA, dB, dC);
+    } else if (blockSize == 16) {
+      dim3 dimBlock((16 * 16) / 8);
+      dim3 dimGrid((N + 16 - 1) / 16, (N + 16 - 1) / 16);
+      Kernels::sgemm1DTiling<16, 16, 8, 8>
+          <<<dimGrid, dimBlock>>>(N, N, N, dA, dB, dC);
+    } else if (blockSize == 32) {
+      dim3 dimBlock((32 * 32) / 8);
+      dim3 dimGrid((N + 32 - 1) / 32, (N + 32 - 1) / 32);
+      Kernels::sgemm1DTiling<32, 32, 8, 8>
+          <<<dimGrid, dimBlock>>>(N, N, N, dA, dB, dC);
     }
   }
-  cudaDeviceSynchronize();
 
   cudaError_t err = cudaGetLastError();
   if (err != cudaSuccess) {
     std::cerr << "CUDA Error: " << cudaGetErrorString(err) << std::endl;
   }
+
   cudaFree(dA);
   cudaFree(dB);
   cudaFree(dC);
