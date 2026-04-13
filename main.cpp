837d2c945c481690fafbb69b46cbd7587a821546
Juan Felipe Rojas <juanfeliperojas2@protonmail.com>
2026-04-13 11:02:12 -0500

    Added basic memory logic

--- main.cpp
+++ main.cpp
@@ -1,20 +1,35 @@
+#include <algorithm>
+#include <array>
 #include <iostream>
+#include <random>
 #include <sycl/sycl.hpp>
 
+template <typename Container> void fillWithRandom(Container &c) {
+  static std::random_device rd;
+  static std::mt19937 gen(rd());
+  using T = typename Container::value_type;
+  std::uniform_real_distribution<T> dis(0.0, 1.0);
+  std::generate(std::begin(c), std::end(c), [&]() { return dis(gen); });
+}
+
 int main() {
-  sycl::queue q;
+  constexpr int N = 512;
+  std::array<float, N * N> A{};
+  fillWithRandom(A);
+  std::array<float, N * N> B{};
+  fillWithRandom(B);
 
-  auto dev = q.get_device();
+  sycl::queue q{sycl::property::queue::in_order()};
 
-  std::cout << "========================================" << std::endl;
-  std::cout << "SYCL Device Info:" << std::endl;
-  std::cout << "Name:     " << dev.get_info<sycl::info::device::name>()
-            << std::endl;
-  std::cout << "Vendor:   " << dev.get_info<sycl::info::device::vendor>()
-            << std::endl;
-  std::cout << "Driver:   "
-            << dev.get_info<sycl::info::device::driver_version>() << std::endl;
-  std::cout << "========================================" << std::endl;
+  float *dA{sycl::malloc_device<float>(N * N, q)};
+  q.submit(
+      [&](sycl::handler &h) { h.memcpy(dA, &A[0], N * N * sizeof(float)); });
+
+  float *dB{sycl::malloc_device<float>(N * N, q)};
+  q.submit(
+      [&](sycl::handler &h) { h.memcpy(dB, &B[0], N * N * sizeof(float)); });
+
+  float *dC{sycl::malloc_device<float>(N * N, q)};
 
   return 0;
 }
