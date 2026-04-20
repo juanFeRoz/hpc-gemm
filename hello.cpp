c356df1005cf196a334a3c00a759e75db7493a10
Juan Felipe Rojas <juanfeliperojas2@protonmail.com>
2026-04-19 21:50:44 -0500

    Updated hello

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,7 @@
 #include <sycl/sycl.hpp>
 
 int main() {
-  sycl::queue q;
+  sycl::queue q{sycl::gpu_selector_v};
   std::cout << q.get_device().get_info<sycl::info::device::name>() << "\n";
   return 0;
 }
