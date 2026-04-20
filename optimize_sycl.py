730cd3f84327d68c1f6a8d7da1d79c81771d251f
Juan Felipe Rojas <juanfeliperojas2@protonmail.com>
2026-04-19 22:18:06 -0500

    Updated sycl opt

--- optimize_sycl.py
+++ optimize_sycl.py
@@ -2,22 +2,25 @@ import optuna
 from wrapper import run
 
 def objective(trial):
+    configs = [
+        {"BM": 8,  "BN": 8,  "BK": 8,  "TM": 1},
+        {"BM": 16, "BN": 16, "BK": 8,  "TM": 2},
+        {"BM": 32, "BN": 32, "BK": 8,  "TM": 4},
+        {"BM": 16, "BN": 16, "BK": 16, "TM": 1},
+        {"BM": 32, "BN": 32, "BK": 16, "TM": 2},
+        {"BM": 32, "BN": 32, "BK": 32, "TM": 1},
+    ]
+    
+    # Let Optuna pick one of the valid indices
+    idx = trial.suggest_int("config_idx", 0, len(configs) - 1)
+    c = configs[idx]
+    
     M, N, K = 512, 512, 512
-    BM = trial.suggest_categorical("BM", [8, 16, 32])
-    BN = trial.suggest_categorical("BN", [8, 16, 32])
-    BK = trial.suggest_categorical("BK", [8, 16, 32])
-    TM = trial.suggest_categorical("TM", [1, 2, 4, 8])
-
-    if BM % TM != 0:
+    res = run(M, N, K, c["BM"], c["BN"], c["BK"], c["TM"])
+    
+    if res < 0:
         return float("inf")
-    if BM != BK * TM:   
-        return float("inf")
-    if BN != BK * TM:  
-        return float("inf")
-    if BK > K:
-        return float("inf")
-
-    return run(M, N, K, BM, BN, BK, TM)
+    return res
 
 study = optuna.create_study(direction="minimize")
 study.optimize(objective, n_trials=50)
